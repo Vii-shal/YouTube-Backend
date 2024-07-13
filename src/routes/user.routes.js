fa736b387c4ca4b8f247f9fe754ff0c005ecb712
Vii-shal <vk19062004@gmail.com>
2024-07-13 19:53:39 +0530

    routes , new controllers , subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from "express"
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js"
+import { 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage 
+} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -25,6 +36,15 @@ router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT,logoutUser)
 router.route('/refresh-token').post(logoutUser)
 
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+router.route('/current-user').get(verifyJWT,getCurrentUser)        // get -> as user is not sending any data in this
+router.route('/update-account').patch(verifyJWT,updateAccountDetails)      // patch -> to update a part
+
+router.route('/update-avatar').patch(verifyJWT,upload.single("avatar"),updateUserAvatar) 
+router.route('/update-cover-image').patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route('/c/:username').get(verifyJWT,getUserChannelProfile)
+router.route('/history').get(verifyJWT,getWatchHistory)
 
 
 export default router
